2b7ad637fd47db31daac7cf55d49b7ee85f4db82
Sam Ratcliffe <sam@stashinvest.com>
2020-12-05 17:39:23 -0500

    Add port number to server listen callback log

--- app.js
+++ app.js
@@ -115,6 +115,6 @@ redis.on('error', (error) => {
 })
 
 if(config.https_enabled) {
-  https.listen(config.ssl_port, '::', () => console.log(`Teddit running on https://${config.domain}`))
+  https.listen(config.ssl_port, '::', () => console.log(`Teddit running on https://${config.domain}:${config.ssl_port}`))
 }
-http.listen(config.nonssl_port, '::', () => console.log(`Teddit running on http://${config.domain}`))
+http.listen(config.nonssl_port, '::', () => console.log(`Teddit running on http://${config.domain}:${config.nonssl_port}`))
