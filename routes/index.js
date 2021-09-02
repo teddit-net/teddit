687857aec63a074853da796c1436931ed59edc45
json <goethe>
2021-09-02 10:27:28 +0100

    separate static routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const overridingRoutes = require('./overides');
+const staticRoutes = require('./static');
 
 const allRoutes = require('express').Router();
 
 allRoutes.use(overridingRoutes);
+allRoutes.use(staticRoutes);
 
 module.exports = allRoutes;
