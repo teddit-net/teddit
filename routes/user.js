f3cc837a01e11652a279e1d60b1bfb0e37a4f67c
json <goethe>
2021-09-07 13:27:29 +0100

    convert processJsonUser to async function

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const config = require('../config');
 const { redis, fetch } = require('../app');
 const userRoutes = require('express').Router();
 
-const processUser = require('../inc/processJsonUser.js')();
+const processJsonUser = require('../inc/processJsonUser.js');
 const processPost = require('../inc/processJsonPost.js')();
 const processAbout = require('../inc/processSubredditAbout.js')();
 const tedditApiUser = require('../inc/teddit_api/handleUser.js')();
