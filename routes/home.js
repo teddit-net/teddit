teddit-net/teddit

commit 3612345fabe380bd327fab284f401d5653c29ee1

proxy i.redd.it and v.redd.it media links in comments (#307)

routes/home.js
@@ -18,15 +18,31 @@ homeRoute.get('/:sort?', async (req, res, next) => {
   let proxyable =
     sortby.includes('.jpg') ||
     sortby.includes('.png') ||
-    sortby.includes('.jpeg')
+    sortby.includes('.jpeg') ||
+    sortby.includes('.mp4') ||
+    sortby.includes('.gif') ||
+    sortby.includes('.gifv')
       ? true
       : false;
   if (proxyable) {
-    let params = new URLSearchParams(req.query).toString();
-    let image_url = `https://preview.redd.it/${sortby}?${params}`;
-    let proxied_image = await downloadAndSave(image_url);
-    if (proxied_image) {
-      return res.redirect(proxied_image);
+    let media_url = '';
+    const replacable_media_domains = ['i.redd.it', 'v.redd.it']
+    if (req.query.teddit_proxy) {
+      if (replacable_media_domains.includes(req.query.teddit_proxy)) {
+        let full_url = req.protocol + '://' + req.get('host') + req.originalUrl;
+        let u = new URL(full_url);
+        let filename = u.pathname || '';
+        let query = u.search || '';
+        media_url = `https://${req.query.teddit_proxy}${filename}${query}`;
+      }
+    } else {
+      let params = new URLSearchParams(req.query).toString();
+      media_url = `https://preview.redd.it/${sortby}?${params}`;
+    }
+
+    let proxied_media = await downloadAndSave(media_url);
+    if (proxied_media) {
+      return res.redirect(proxied_media);
     } else {
       return res.redirect('/');
     }

inc/commons.js
@@ -195,7 +195,54 @@ module.exports = function(request, fs) {
     let instagramRegex = /(?<=href=")(https?:\/\/)(www+\.)?instagram.com(?=.+")/gm;
 
     let protocol = config.https_enabled || config.api_force_https ? 'https://' : 'http://'
-    
+
+    /**
+     * Special handling for reddit media domains in comments hrefs.
+     * For example a comment might have a direct links to images in i.redd.it:
+     * <a href="https://i.redd.it/hly9gyg9gjh81.png">Just refer to this </a>
+     * We want to rewrite these hrefs, but we also need to include the media
+     * for our backend, so we know where to fetch the media from.
+     * That comment URL then becomes like this after rewriting, for example:
+     * <a href="https://teddit.net/hly9gyg9gjh81.png?teddit_proxy=i.redd.it">Just refer to this </a>
+     * And then in our backend, we check if we have a 'teddit_proxy' in the req
+     * query, and proceed to proxy if it does.
+     */
+    const replacable_media_domains = ['i.redd.it', 'v.redd.it', 'preview.redd.it']
+    replacable_media_domains.forEach((domain) => {
+      if (str.includes(domain + "/")) {
+        const href_regex = new RegExp(`(?<=href=")(https?:\/\/)([A-z.]+\.)?(${domain})(.+?(?="))`, 'gm')
+        const hrefs = str.match(href_regex)
+        if (!hrefs) {
+          return
+        }
+
+        hrefs.forEach((url) => {
+          let original_url = url
+          const valid_exts = ['png', 'jpg', 'jpeg', 'mp4', 'gif', 'gifv']
+          const file_ext = getFileExtension(url)
+          if (valid_exts.includes(file_ext)) {
+            url = url.replace(domain, config.domain)
+
+            // append the domain info to the query, for teddit backend
+            let u = new URL(url)
+            if (u.query) {
+              url += '&teddit_proxy=' + domain
+            } else {
+              url += '?teddit_proxy=' + domain
+            }
+
+            // also replace the protocol for instances using http only
+            if (protocol === 'http://' && u.protocol === 'https:') {
+              url.replace('https://', protocol)
+            }
+            str = str.replace(original_url, url)
+          }
+        })
+      }
+    })
+
+    // Continue the normal replace logic
+
     str = str.replace(redditRegex, protocol + config.domain)
     
     if(typeof(user_preferences) == 'undefined')

commit 8ccc694d2f85a13842cf96afbed9246374608a03

fix media proxying (clicking preview.redd.it image links was broken after 3612345fab

routes/home.js
@@ -38,6 +38,10 @@ homeRoute.get('/:sort?', async (req, res, next) => {
     } else {
       let params = new URLSearchParams(req.query).toString();
       media_url = `https://preview.redd.it/${sortby}?${params}`;
+      if (media_url.includes('teddit_proxy')) {
+        // if the URL includes teddit_proxy query param, remove everything after it
+        media_url = media_url.split('%3Fteddit_proxy')[0];
+      }
     }
 
     let proxied_media = await downloadAndSave(media_url);